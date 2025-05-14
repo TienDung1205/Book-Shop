df4bcd8434309af3b6a17371570c011ac59321b5
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2025-05-14 19:58:21 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,19 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        deleted: false,
+        status: "active"
+    });
+
+    const newProducts = products.map(item => {
+        item.newPrice = (item.price - item.discountPercentage).toFixed(0);
+        return item;
+    })
+
     res.render("client/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm"
+        pageTitle: "Danh sách sản phẩm",
+        products: newProducts
     })
 }
